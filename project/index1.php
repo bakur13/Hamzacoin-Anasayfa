f936f8005be841a3f2d091125559a5bdac1ad3cd
Hamza Uslu <hamzauslu056@gmail.com>
2022-11-27 17:19:03 +0300

    Hamzacoin Anasayfa
    Techcareer eğitim bootcamp  proje ödevi kapsamında html,css,php ve sql kullanarak yaptığım Hamzacoin Anasayfa

---
+++ project/index1.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+       
+      <meta charset="UTF-8">
+      <title>HAMZA COİN </title>
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+      <script src="https://kit.fontawesome.com/77d7a14a17.js" crossorigin="anonymous"></script>
+    
+    
+     <link rel="stylesheet" href="owl/owl.carousel.min.css">
+  <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+    
+    
+ </head>
+<body>
+    <section id="Menu">
+       <div id ="logo">HAMZA COİN</div>
+        <nav>
+               <a href=""><i class="fa-solid fa-house-user ikon"></i>ANASAYFA</a>
+               <a href=""><i class="fa-solid fa-info ikon"></i>HAKKIMIZDA</a>
+               <a href=""><i class="fa-solid fa-people-group ikon"></i>EKİP</a>
+               <a href=""><i class="fa-solid fa-address-book ikon"></i>İLETİŞİM</a>
+               <a href=""><i class="fa-brands fa-watchman-monitoring"></i>EKOSİSTEM</a>
+        
+        </nav>
+    
+    
+    </section>
+    
+    
+    <section id="ANASAYFA">
+         
+        <div id="black"> </div>
+         
+       
+        
+    <div id="icerik">
+        
+        <h2>HMZ COİN</h2>
+        
+        <hr align=left width=300>
+        
+        <p>
+            Used by millions
+        Applications built on HMZ technology are already used by millions of people to label data and prevent bot abuse, enabling next generation AI and ML technologies. The HMZ open-source community is now working to support many other apps and use cases.
+     </p>
+   
+     <hr align=left width=300>
+        
+        <p>Requesters those wishing to have work completed -- choose a template or customized job type, and create a job specification. Jobs are intelligently split into hundreds or thousands of tasks that are distributed to supporting Job Exchanges. These tasks are then assigned to an appropriate human or machine Worker based on 
+        the job’s requirements.
+    </p>
+        
+    <hr align=left width=300>
+        
+    <p> 
+    
+       A job could be split into thousands of tasks, and distributed by supporting Job Exchanges to relevant Workers. If the Requester requires a dataset created by Workers from a particular ethnic, cultural, or professional background, that can be defined in the job.
+   </p>
+   
+        </div>
+        
+      </section>
+      
+     <section id="hakkimizda">
+          
+         <h3>HAKKIMIZDA</h3>
+        
+         <div class="container">
+              
+         <div id="sol">
+              <h5 id="h5sol" > Hamza Uslu Junior Software Engineer 
+              
+            İSTANBUL MEDENİYET UNİVERSTY
+                  </h5>
+                  
+             </div> 
+          <div id="sag">
+              
+              <span>L</span>
+              <p id="psag">et us tell you about us, HAMZA PROTOCOL. If you’ve sho... </p>
+              
+          </div>
+         
+              <img src="img/block-3.jpg" alt="" class="img-fluit mt100">
+              
+              <p id="pson">
+                  HAMZA’s vision for a tokenized work, knowledge, and contribution platform combined with Blockchain Helix's identity solution is a great fit for a global, frictionless  and a centric app for Workers. We see tremendous potential in the power of a global, freelance machine learning and algorithm training platform.” – Oliver Naegele, Founder  CEO of Blockchain HELIX
+            </p>  
+          </div>
+     </section>
+    
+    <section id="PROJECTS">
+      
+         <div class="container">
+             
+               <h3>PROJECTS</h3>
+             
+             <div class="owl-crousel owl-theme">
+             
+                 <div class="card item">
+                 
+                     <img src="img/block-4.jpg" alt=""class="img-fluid">
+                     
+                     <h5 class="baslikcard"> High-throughput</h5>
+                     
+                     <p class="cardp">
+                         
+                         The high throughput of HMZ, underpinned by improved network TPS, makes HMZ a better option for day-to-day use than Bitcoin and Ethereum.
+                     </p>
+                     
+                 </div>
+                   <div class="card item">
+                 
+                     <img src="img/block-3.jpg" alt=""class="img-fluid">
+                     
+                     <h5 class="baslikcard"> High-scalability</h5>
+                     
+                     <p class="cardp">
+                         
+                         Strong scalability and smart contracts allow applications to be deployed on HMZ in diverse ways, enabling.
+                     </p>
+                     
+                 </div>
+                   <div class="card item">
+                 
+                     <img src="img/block-5.jpg" alt=""class="img-fluid">
+                     
+                     <h5 class="baslikcard"> High-availability</h5>
+                     
+                     <p class="cardp">
+                         
+                         HMZPROTOCOL has a more reliable network structure, user assets, and intrinsic value.
+                     </p>
+                     
+                 </div>
+                 
+             
+             
+             </div>
+         
+         </div>
+
+    </section>
+    
+    <section id="ekip">
+        
+         <div class="container">
+             <h3 id="ekiph3">TEAM</h3>
+             
+             <div class="sutun-sol-sag">
+                 <img src="img/s%C3%BCleyman2.jpg" alt="" class="img-fluid oval">
+                 <h4 class="ekipisim">SÜLEYMAN TAŞ</h4>
+                 <p class="ekipp">
+                
+                      Digital Art Designer 
+                     Had been experience for 8 years 
+                 
+                 </p>
+             
+                  <div class="ekip-ikon">
+                      <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                      <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                      <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                         
+                  
+                  </div>
+             </div>
+             <div class="sutun">
+                 <img src="img/hamza2.jpg" alt="" class="img-fluid oval">
+                 <h4 class="ekipisim">HAMZA USLU</h4>
+                 <p class="ekipp">
+                
+                      Software Engineer
+                 
+                 </p>
+             
+                  <div class="ekip-ikon">
+                      <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                      <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                      <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                         
+                  
+             </div>
+             </div>
+             <div class="sag">
+                 <img src="img/yunus.jpg" alt="" class="img-fluid oval">
+                 <h4 class="ekipisim">YUNUS KABUL</h4>
+                 <p class="ekipp">
+                
+                      Human Resources Manager            
+                     Had been experience for 10 years 
+                 
+                 </p>
+             
+                  <div class="ekip-ikon">
+                      <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                      <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                      <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                         
+                  
+                  </div>
+             </div>
+        </div>
+        
+    </section>
+    
+    <section id="iletisim">
+        <div class="container">
+            
+            <h3 id="h3iletisim"> İLETİŞİM</h3>
+            
+            <form action="index.php" method="post">
+            
+         <div id="iletisimopak">
+          <div id="formgroup">
+              <div id="solform">
+                 <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+                 <input type="text" name="tel" placeholder="Telefon numarası" required class="form-control">
+                  
+             </div>    
+              <div id="sagform">
+                  <input type="email" name="mail" placeholder="Email Adresiniz" required class="form-control">
+                 <input type="text" name="Konu" placeholder="Konu Başlığı" required class="form-control">
+               </div>
+            
+             <textarea name="mesaj" id="" cols="30" placeholder="Mesaj Giriniz" rows="10" required class="form-control"></textarea>
+              
+             <input type="submit" value="Gönder">
+     </div>
+             
+            <div id="adres">
+              
+                 <h4 id="adresbaslik">Adres :</h4>
+                 <p>Cumhuriyet mah.</p>
+                 <p>Kizir caddesi</p>
+                 <p>1925 sokak</p>
+                 <p>05415711925</p>
+            <p>Email:hamzauslu056@gmail.com</p>
+                 
+             
+           </div>
+          </div>  
+                
+            </form>
+            <footer>
+                
+              <div id="copyright">2022|Tüm Hakları saklıdır</div>
+                <div id="socialfooter">
+                <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                      <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                      <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                         
+                </div>
+                <a href="#menu"><i class="fa-sharp fa-solid fa-chevron-up " id="up"></i></a>
+            </footer>
+        
+        </div>
+    </section>
+    
+    
+      <script
+     src="https://code.jquery.com/jquery-3.6.1.slim.min.js"
+     integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA="
+       crossorigin="anonymous"></script>
+    
+    <script src="owl/owl.carousel.min.js"></script>
+    
+    <script src="owl/script.js"></script>
+</body>
+
+</html>
+
+
+<?php 
+
+include("baglanti.php");
+
+if(isset($_POST["isim"],$_POST["tel"],$_POST["mail"],$_POST["konu"],$_POST["mesaj"])){
+    
+    $adsoyad=$_POST["isim"];
+    $telefon=$_POST["tel"];
+    $email=$_POST["mail"];
+    $konu=$_POST["konu"];
+    $mesaj=$_POST["mesaj"];
+    
+    $ekle="INSERT INTO iletisim( Adsoyad, telefon, email, konu, mesaj) VALUES ('".$adsoyad."','".$telefon."','".$email."','".$konu."','".$mesaj."')";
+    
+    if($baglan->query($ekle)===TRUE){
+        
+        echo "<script>alert('Mesajınız Gönderildi.')</script>";
+    }
+    else{
+        echo "<script>alert('HATA.')</script>";
+    }
+}
+
+
+
+?>
